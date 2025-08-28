859035c0771de67f4466392b3b540bc0a4813440
Liqun Li <liqlee@gmail.com>
2025-08-27 20:54:14 -0700

    add an asyncio sample code

---
+++ aio.py
@@ -0,0 +1,22 @@
+
+import asyncio
+import time
+
+async def play () :
+  print('enter play')
+  for _ in range(2) :
+    await asyncio.sleep(1)
+    for _ in range(1000) :
+      pass
+
+async def main() :
+  print('enter main')
+  start = time.time()
+  tasks = []
+  for _ in range(10) :
+    tasks.append(play())
+  await asyncio.gather(*tasks)
+  print(f'total time : {time.time() - start} seconds')
+
+# main()
+asyncio.run(main())
